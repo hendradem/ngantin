54410fa86db007a08f48efbd77ea72260ad35aa0
Ade Mahendra <hendraadem@gmail.com>
2023-06-17 08:19:35 +0700

    Fix: CORS Issues

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -21,20 +21,18 @@ export const signUp = (user) => {
 };
 
 export const signIn = (email, password) => {
+  const config = {
+    method: "post",
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "POST, GET, PUT",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  };
+
   return (dispatch) => {
     axios
-      .post(
-        `${url}/login`,
-        {
-          method: "post",
-          headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "POST, GET, PUT",
-            "Access-Control-Allow-Headers": "Content-Type, Authorization",
-          },
-        },
-        { email, password }
-      )
+      .post(`${url}/login`, { email, password }, config)
       .then((res) => {
         localStorage.setItem("token", res.data.token);
 
